18468a49799da52d866a2a3a9008f92e7142439f
Gabriel Valente Perinetti <evalesoftware@gmail.com>
2018-04-12 13:13:25 -0300

    commit 1

---
+++ app/src/main/java/com/devmasterteam/festafimdeano/views/DetailsActivity.java
@@ -0,0 +1,62 @@
+package com.devmasterteam.festafimdeano.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+
+import com.devmasterteam.festafimdeano.R;
+import com.devmasterteam.festafimdeano.constants.FimDeAnoConstants;
+import com.devmasterteam.festafimdeano.util.SecurityPrefences;
+
+public class DetailsActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ViewHolder mViewHolder = new ViewHolder();
+    private SecurityPrefences mSecurityPrefences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
+
+        this.mSecurityPrefences = new SecurityPrefences(this);
+
+        this.mViewHolder.checkParticipate = (CheckBox) findViewById(R.id.check_participate);
+
+        this.mViewHolder.checkParticipate.setOnClickListener(this);
+
+        this.loadDataFromActivity();
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if (id == R.id.check_participate) {
+            if (this.mViewHolder.checkParticipate.isChecked()) {
+                this.mSecurityPrefences.storeString(FimDeAnoConstants.PRESENCE, FimDeAnoConstants.CONFIRMED_WILL_GO);
+            } else {
+                this.mSecurityPrefences.storeString(FimDeAnoConstants.PRESENCE, FimDeAnoConstants.CONFIRMED_WONT_GO);
+            }
+        }
+    }
+
+    private void loadDataFromActivity() {
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String presence = extras.getString(FimDeAnoConstants.PRESENCE);
+            if (presence.equals(FimDeAnoConstants.CONFIRMED_WILL_GO)) {
+                this.mViewHolder.checkParticipate.setChecked(true);
+            } else {
+                this.mViewHolder.checkParticipate.setChecked(false);
+            }
+        }
+    }
+
+    private static class ViewHolder {
+        CheckBox checkParticipate;
+    }
+}
